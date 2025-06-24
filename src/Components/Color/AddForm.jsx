adcea28657dacb12bdecfadbc03d4f5d5797b893
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-24 15:25:42 +0200

    adding Form adds colors

--- src/Components/Color/AddForm.jsx
+++ src/Components/Color/AddForm.jsx
@@ -1,16 +1,28 @@
+import { uid } from "uid";
+
 export default function AddForm({ handleAdd }) {
+  const inputs = ["role", "hex", "contrastText"];
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target)
-    console.log(data.get('role'));
-    const role = data.get('role')
-    
-    handleAdd(role);
+    const data = new FormData(e.target);
+    console.log(data.get("role"));
+    // const role = data.get("role");
+    let [role, hex, contrastText] = inputs.map((item) => data.get(item));
+    hex[0] !== "#" && (hex = "#" + hex);
+    handleAdd(role, hex, contrastText);
   };
+
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="role"></label>
-      <input type="text" id="role" name="role"/>
+      {inputs.map((item) => {
+        return (
+          <div key={uid()}>
+            <label htmlFor={item} />
+            <input placeholder={item} type="text" id={item} name={item} />
+          </div>
+        );
+      })}
       <button type="submit">submit</button>
     </form>
   );
