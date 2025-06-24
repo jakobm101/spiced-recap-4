78dd2933792313b71952c80f516c1bd7c67ce56b
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-24 14:44:21 +0200

    adding color with button possible

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  console.log(initialColors);
   const [colors, setColors] = useState(initialColors);
+
+  const addColor = () => {
+    setColors([
+      { id: 1234, role: "test", hex: "#435632", contrastText: "#ffffff" },
+      ...colors,
+    ]);
+    console.log(colors);
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
+      <button onClick={addColor}>add color</button>
 
       {colors.map((color) => {
         return <Color key={color.id} color={color} />;
