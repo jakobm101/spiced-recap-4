150fb72c8d0795b67c17ead9c73a3dece477a778
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-24 14:37:11 +0200

    colors got state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  console.log(initialColors);
+  const [colors, setColors] = useState(initialColors);
   return (
     <>
       <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
